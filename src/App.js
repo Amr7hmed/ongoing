8ba0c513be01899bcc987d9778fc84cd8ac1e0c0
Your Name <amr7hmed@gmail.com>
2022-03-24 14:30:48 +0200

    Update React Project

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.scss";
 import Home from "./pages/Home/index.jsx";
 import Navbar from "./components/Navbar/index.jsx";
 import Footer from "./components/Footer/index.jsx";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter , Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Navbar/>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
-      </Router>
+      </BrowserRouter>
       <Footer />
     </>
   );
